1b386b55962501d26cf3fc8c1722d024b9068475
dat-n <20078201@tafe.wa.edu.au>
2022-05-19 15:45:19 +0800

    birthday calculator part 1 complete

--- ReadMe.md
+++ ReadMe.md
@@ -21,4 +21,11 @@ inspirations are shown below:
 - Site Name: https://www.w3schools.com/howto/howto_js_dropdown.asp
 - Site Name: https://ricardometring.com/getting-the-value-of-a-select-in-javascript
 - Site Name: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label
+- Site Name: https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
+- Site Name: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#interpretation_of_two-digit_years
+- Site Name: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+- Site Name: https://www.codegrepper.com/code-examples/javascript/round+of+2+decimal+places+in+javascript
+- Site Name: https://www.geeksforgeeks.org/how-to-calculate-days-left-until-next-christmas-using-javascript/
+- Site Name: 
+- Site Name: 
 - Site Name: 
--- assets/js/demo-02.js
+++ assets/js/demo-02.js
@@ -6,6 +6,92 @@
 *
 *
 * */
+let footerContent = document.getElementById("card_footer")
+
+let age = document.getElementById("age")
+let days = document.getElementById("daysLeftTillBirthday")
+
+button = document.getElementById("calculate")
+
+button.addEventListener('click', () => {
+    let day = document.getElementById("Day")
+    let dayValue = day.options[day.selectedIndex].value
+
+    let month = document.getElementById("Month")
+    let monthValue = month.options[month.selectedIndex].value
+
+    let year = document.getElementById("Year")
+    let yearValue = year.options[year.selectedIndex].value
+
+    let remainder = yearValue%4
+
+    if (remainder === 0 && monthValue === 2){
+        footerContent.innerText = "Feb has 29 days during a leap"
+        return
+    }
+    console.log(yearValue % 4, monthValue)
+    // Time object for current date
+    let today = new Date()
+
+    // Time object with birthday
+    let birthdayDate = new Date(`${yearValue},${monthValue},${dayValue}`)
+
+    // set the years, months, days  to correct value
+    let currentYear = today.getFullYear()
+    let currentMonth = today.getMonth() + 1
+    let currentDay = today.getDate()
+
+    let birthdayYear = birthdayDate.getFullYear()
+    let birthdayMonth = birthdayDate.getMonth()
+    let birthdayDay = birthdayDate.getDate()
+    
+    let upcomingBDay = new Date(currentYear, birthdayMonth, birthdayDay)
+
+    const one_day = 1000 * 60 * 60 * 24
+
+    // Verification of the date the user entered (leap year etc)
+
+    if (currentMonth === birthdayMonth && currentDay === birthdayDay){
+        footerContent.innerText = "Happy Birthday! 🎂"
+    }
+    
+    if (today > upcomingBDay){
+        upcomingBDay.setFullYear(today.getFullYear() + 1)
+    }
+
+    let remainingDaysTillBirthday = Math.ceil((upcomingBDay.getTime() - today.getTime()) / one_day)
+
+    // Get Years
+    let ageYear = currentYear - birthdayYear
+    let ageMonth, ageDays
+
+    // Get Months
+    if (currentMonth >= birthdayMonth){
+        // get months when current month is greater
+        ageMonth = currentMonth - birthdayMonth
+    } else {
+        ageYear--
+        ageMonth = 12 + currentMonth + birthdayMonth
+    }
+
+    // Get days
+    if (currentDay >= birthdayDay){
+        ageDays = currentDay - birthdayDay
+    } else {
+        ageMonth--
+        ageDays = 31 + currentDay - birthdayDay
+    }
+
+    if (ageMonth < 0){
+        ageMonth = 11
+        ageYear--
+    }
+
+    age.innerText = `You are ${ageYear} years, ${ageMonth} months and ${ageDays} days young`
+    days.innerText = `${remainingDaysTillBirthday} days until your birthday`
+
+})
+
 
 
 
